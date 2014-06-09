45c681e62c5521d6d3d6c809ef78aabe10614bce
hubuntub <begona26@gmail.com>
2014-06-09 18:21:24 +0200

    uploading

--- src/main/java/com/ge/prototype/sample/server/FileServletUpload.java
+++ src/main/java/com/ge/prototype/sample/server/FileServletUpload.java
@@ -1,5 +1,6 @@
 package com.ge.prototype.sample.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
@@ -14,52 +15,68 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 public class FileServletUpload extends HttpServlet {
 
-	  private long FILE_SIZE_LIMIT = 20 * 1024 * 1024; // 20 MiB
-	  private final Logger logger = Logger.getLogger("UploadServlet");
+	private long FILE_SIZE_LIMIT = 20 * 1024 * 1024; // 20 MiB
+	private final Logger logger = Logger.getLogger("UploadServlet");
 
-	  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-	    throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 
-	    try {
-	      DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
-	      ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
-	      fileUpload.setSizeMax(FILE_SIZE_LIMIT);
+		try {
+			DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
+			ServletFileUpload fileUpload = new ServletFileUpload(
+					fileItemFactory);
+			fileUpload.setSizeMax(FILE_SIZE_LIMIT);
+			Config conf = ConfigFactory.load();
+			String path = conf.getString("upload.path");
+			File folder = new File(path);
 
-	      List<FileItem> items = fileUpload.parseRequest(req);
+			if (!folder.exists()) {
+				try {
+					folder.mkdir();
 
-	      for (FileItem item : items) {
-	        if (item.isFormField()) {
-	          logger.log(Level.INFO, "Received form field:");
-	          logger.log(Level.INFO, "Name: " + item.getFieldName());
-	          logger.log(Level.INFO, "Value: " + item.getString());
-	        } else {
-	          logger.log(Level.INFO, "Received file:");
-	          logger.log(Level.INFO, "Name: " + item.getName());
-	          logger.log(Level.INFO, "Size: " + item.getSize());
-	        }
+				} catch (SecurityException e) {
 
-	        if (!item.isFormField()) {
-	          if (item.getSize() > FILE_SIZE_LIMIT) {
-	            resp.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE,
-	              "File size exceeds limit");
+				}
+			}
+			List<FileItem> items = fileUpload.parseRequest(req);
 
-	            return;
-	          }
+			for (FileItem item : items) {
+				if (item.isFormField()) {
+					logger.log(Level.INFO, "Received form field:");
+					logger.log(Level.INFO, "Name: " + item.getFieldName());
+					logger.log(Level.INFO, "Value: " + item.getString());
+				} else {
+					logger.log(Level.INFO, "Received file:");
+					logger.log(Level.INFO, "Name: " + item.getName());
+					logger.log(Level.INFO, "Size: " + item.getSize());
+				}
 
-	          // Typically here you would process the file in some way:
-	          // InputStream in = item.getInputStream();
-	          // ...
+				if (!item.isFormField()) {
+					if (item.getSize() > FILE_SIZE_LIMIT) {
+						resp.sendError(
+								HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE,
+								"File size exceeds limit");
 
-	          if (!item.isInMemory())
-	            item.delete();
-	        }
-	      }
-	    } catch (Exception e) {
-	      logger.log(Level.SEVERE, "Throwing servlet exception for unhandled exception", e);
-	      throw new ServletException(e);
-	    }
-	  }
+						return;
+					}
 
-	}  
+					// Typically here you would process the file in some way:
+
+					File uploadedFile = new File(folder, item.getName());
+					item.write(uploadedFile);
+
+				}
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,
+					"Throwing servlet exception for unhandled exception", e);
+			throw new ServletException(e);
+		}
+	}
+
+}
